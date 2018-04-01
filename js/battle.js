dc9f73c376b899def5fa587ecee56a78d90380ba
Clément Dubrisay <clement.dubrisay@gmail.com>
2018-04-02 01:03:26 +0200

    Adding score, health bar and first enemy

--- js/battle.js
+++ js/battle.js
@@ -4,48 +4,71 @@
 
 var shots = [
     { 
-        indx :      0,
-        strenght:  10,
+        indx : 0,
+        strenght: 10,
         posX: 0,
-        posY:0
+        posY: 0
     },
     { 
-        indx :      1,
-        strenght:  10,
+        indx : 1,
+        strenght: 10,
         posX: 0,
-        posY:0
+        posY: 0
     },
     { 
-        indx :      2,
-        strenght:  10,
+        indx : 2,
+        strenght: 10,
         posX: 0,
-        posY:0
+        posY: 0
     },
     { 
-        indx :      3,
-        strenght:  10,
+        indx : 3,
+        strenght: 10,
         posX: 0,
-        posY:0
+        posY: 0
     },
     { 
-        indx :      4,
-        strenght:  10,
+        indx : 4,
+        strenght: 10,
         posX: 0,
-        posY:0
+        posY: 0
     },
     { 
-        indx :      5,
-        strenght:  10,
+        indx : 5,
+        strenght: 10,
         posX: 0,
-        posY:0
+        posY: 0
     }
 ];
 
-var body = $('body');
+var enemies = [
+    { 
+        name : "trooper",
+        strenght: 1,
+        health: 20,
+        posX: 0,
+        posY: 0,
+        width:70,
+        height:70
+    }
+];
+
+// var troopers = [
+//     { 
+//         strenght: 1,
+//         health: 20,
+//         posX: 0,
+//         posY: 0,
+//         width:70,
+//         height:70
+//     }
+// ];
 
 //////////////////////////////////////////////////////////////////////////////
 // The Space ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
+
+var body = $('body');
 
 var spaceBg = $('.gameSpace');
 var spaceHeight = 600;
@@ -56,11 +79,13 @@ var bg2  = $('.bg2');
 var bg1X = 0;
 var bg2X = 0;
 
+var score = 0;
+
 // Background scrolling //////////////////////////////////////////////////////
 var scrollSpeed = 40;
-
+var scrollInterval;
 function scrolling(){
-    var scrollInterval = setInterval(function(){
+    scrollInterval = setInterval(function(){
         if(bg1X > -1000){
             bg1X -= 1;
             bg1.css({'background-position-x':bg1X});
@@ -90,12 +115,12 @@ scrolling();
 ////////////////////////////////////////////////////////////////////////////
 
 // Creation de l'instance du vaisseau ////////////////////////////////////////
-var myShip = new Ship_obj(150, 150);
+var myShip = new Ship_obj(150, 150, shots);
 
 // Init ship position ////////////////////////////////////////////////////////
 myShip.ship.css({'background-position-y':-myShip.shipBgCurrent});
-myShip.ship.css({'top':myShip.shipPosYcurrent});
-myShip.ship.css({'left':myShip.shipPosXcurrent});
+myShip.ship.css({'top':myShip.posY});
+myShip.ship.css({'left':myShip.posX});
 
 // Intervals for ship moves //////////////////////////////////////////////////
 var shipBgInterval;
@@ -105,13 +130,21 @@ var shipPosXInterval;
 // Shots /////////////////////////////////////////////////////////////////////
 // var shot = $('.shot');
 
-var shot = new Shots(shots);
+var ammunitions = [
+    new Shots(10, 0, 0, 10, 4),
+    new Shots(10, 0, 0, 10, 4),
+    new Shots(10, 0, 0, 10, 4),
+    new Shots(10, 0, 0, 10, 4),
+    new Shots(10, 0, 0, 10, 4),
+    new Shots(10, 0, 0, 10, 4)
+];
+
 var shotIdx = 0;
 var currentShot = 0;
 var shotHeight = 4;
 var loadingShot;
 var shotPosition = setInterval(function(){
-    $('.armed').css({'top':myShip.shipPosYcurrent+(myShip.height/2), 'left':myShip.shipPosXcurrent+(myShip.width)});
+    $('.armed').css({'top':myShip.posY+(myShip.height/2), 'left':myShip.posX+(myShip.width)});
 }, 8);
 
 
@@ -119,12 +152,12 @@ var shotPosition = setInterval(function(){
 $('body').keydown(function() {
     switch(event.keyCode){
         case 32:
-            shot.arm();
+        ammunitions[shotIdx].arm();
             break;
 
         case 104:
         case 38:
-            console.log("Up");
+            // console.log("Up");
             clearInterval(shipBgInterval);
             shipBgInterval = setInterval(function(){
                 myShip.moveBgUp();
@@ -138,7 +171,7 @@ $('body').keydown(function() {
         
         case 98:
         case 40:
-            console.log("Down");
+            // console.log("Down");
             clearInterval(shipBgInterval);
             shipBgInterval = setInterval(function(){
                 myShip.moveBgDown();
@@ -152,7 +185,7 @@ $('body').keydown(function() {
 
         case 100:
         case 37:
-            console.log("Left");
+            // console.log("Left");
             clearInterval(shipPosXInterval);
             shipPosXInterval = setInterval(function(){
                 myShip.moveLeft();
@@ -161,26 +194,26 @@ $('body').keydown(function() {
 
         case 102:
         case 39:
-            console.log("Right");
+            // console.log("Right");
             clearInterval(shipPosXInterval);
             shipPosXInterval = setInterval(function(){
                 myShip.moveRight();
             }, 8);
             break;
     }
-    console.log(event.keyCode);
+    // console.log(event.keyCode);
 });
 
 // KEYUP /////////////////////////////////////////////////////////////////////
 $('body').keyup(function() {
     switch(event.keyCode){
         case 32:
-            shot.fire();
+        ammunitions[shotIdx].fire();
             break;
 
         case 104:
         case 38:
-            console.log("Release Up");
+            // console.log("Release Up");
             clearInterval(shipBgInterval);
             clearInterval(shipPosYInterval);
             shipBgInterval = setInterval(function(){
@@ -190,7 +223,7 @@ $('body').keyup(function() {
         
         case 98:
         case 40:
-            console.log("Release Down");
+            // console.log("Release Down");
             clearInterval(shipBgInterval);
             clearInterval(shipPosYInterval);
             shipBgInterval = setInterval(function(){
@@ -208,25 +241,202 @@ $('body').keyup(function() {
             clearInterval(shipPosXInterval);
             break;
     }
-    console.log(event.keyCode);
+    // console.log(event.keyCode);
 });
 
 
+//////////////////////////////////////////////////////////////////////////////
+// The Enemies //////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+// var trooper = new Trooper(troopers, 1, 20, 100, 100, 70, 70);
+
+// trooper.sayMyName();
+// trooper.launch(1000, 265);
+
+var trooperSection = [
+    // new Trooper(1, 20, 1000, -70, 70, 70),
+    // new Trooper(1, 20, 1000, -70, 70, 70),
+    // new Trooper(1, 20, 1000, -70, 70, 70),
+    // new Trooper(1, 20, 1000, -70, 70, 70),
+    // new Trooper(1, 20, 1000, -70, 70, 70),
+    // new Trooper(1, 20, 1000, -70, 70, 70)
+
+    new Trooper(1, 20, 1000, -70, 0),
+    new Trooper(1, 20, 1000, -70, 1),
+    new Trooper(1, 20, 1000, -70, 2),
+    new Trooper(1, 20, 1000, -70, 3),
+    new Trooper(1, 20, 1000, -70, 4),
+    new Trooper(1, 20, 1000, -70, 5)
+];
+
+var trooperIdx = 0;  
+
+// Troopers launching
+var trooperLaunching = setInterval(function(){
+    if(trooperIdx < 5){
+        trooperSection[trooperIdx].launch(1000, 265);
+        trooperIdx += 1;
+    }
+    else{
+        trooperSection[trooperIdx].launch(1000, 265);
+        trooperIdx = 0;
+    }
+    
+}, 1000);
+
+// clearInterval(trooperLaunching);
 
 
+//////////////////////////////////////////////////////////////////////////////
+// Collisions ///////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
 
+function getTop (obj) {
+    return obj.posY;
+}
 
+function getBottom (obj) {
+    return obj.posY + obj.height;
+}
 
+function getLeft (obj) {
+    return obj.posX;
+}
 
+function getRight (obj) {
+    return obj.posX + obj.width;
+}
 
+function collision (objA, objB) {
+    // console.log("posX : " + objB.posX);
+    // console.log("posY : " + objB.posY);
+    // console.log("Width : " + objB.width);
+    // console.log("Height : " + objB.height);
+    return  getBottom(objA) >= getTop(objB)    &&
+            getTop(objA)    <= getBottom(objB) &&
+            getRight(objA)  >= getLeft(objB)   &&
+            getLeft(objA)   <= getRight(objB);
+}
 
+function collisionShip (objA, objB) {
+    return  (getBottom(objA)-65) >= getTop(objB)    &&
+            (getTop(objA)+65)    <= getBottom(objB) &&
+            (getRight(objA)-35)  >= getLeft(objB)   &&
+            (getLeft(objA)+35)   <= getRight(objB);
+}
 
+// function trooperCollision () {
+//     var hasCollided = false;
 
+//     trooperSection.forEach(function (oneTrooper) {
+//         if (collision(myShip, oneTrooper)) {
+//             hasCollided = true;
+//             console.log("BOOM");
+//         }
+//     });
 
+//     return hasCollided;
+// }
 
+function trooperCollision (oneTrooper) {
+    var hasCollided = false;
+    // console.log(oneTrooper);
+    
+    // console.log(oneTrooper.posX);
+    // console.log(oneTrooper.posY);
 
+    if (collisionShip(myShip, oneTrooper)) {
+        hasCollided = true;
+        // console.log("BOOM");
+    }
+    // console.log(hasCollided);
 
+    return hasCollided;
+}
 
+function trooperExplosion(index){
+    var explosion = '<div index="' + index + '" class="explosion"></div>';
+    $('.explosionsLayer').append(explosion);
+    explosion = $('.explosion[index='+ index +']');
+    explosion.css({'left':trooperSection[index].posX-35, 'top':trooperSection[index].posY-35});
+    var explosionBg = 0;
+    var explosionInterval = setInterval(function(){
+        explosion.css({'background-position-y':explosionBg});
+        explosionBg -= 140;
+        if(explosionBg <= -2380){
+            clearInterval(explosionInterval);
+            explosion.remove();
+        }
+    },60);
+}
+
+function sparkles(index){
+    var sparkle = '<div index="' + index + '" class="sparkle"></div>';
+    $('.explosionsLayer').append(sparkle);
+    sparkle = $('.sparkle[index='+ index +']');
+    sparkle.css({'left':trooperSection[index].posX-70, 'top':trooperSection[index].posY});
+    var sparkleBg = 0;
+    var sparkleInterval = setInterval(function(){
+        sparkleBg -= 70;
+        sparkle.css({'background-position-y':sparkleBg});
+        if(sparkleBg <= -350){
+            clearInterval(sparkleInterval);
+            sparkle.remove();
+        }
+    },30);
+}
+
+function shotCollision (oneShot) {
+    var hasCollided = false;
+
+    trooperSection.forEach(function (oneTrooper) {
+        if (collision(oneShot, oneTrooper)) {
+            hasCollided = true;
+            // console.log("HIT!");
+            console.log("Index : " + oneTrooper.idx);
+
+            if(oneTrooper.receiveDamage(oneShot) <= 0){
+                var thisTrooper = $('.trooper[index='+ oneTrooper.idx +']');
+                thisTrooper.css({'bottom':2000});
+                
+                thisTrooper.stop();
+                thisTrooper.css({'left': 1000, 'top': -70, 'background-position-y':0, 'background-position-x':0}).hide();
+                
+                trooperExplosion(oneTrooper.idx);
+
+                oneTrooper.posX = 1000;
+                oneTrooper.posY = -70;
+                oneTrooper.health = 20;
+
+                increaseScore(oneTrooper.health);
+            }
+            else{
+                sparkles(oneTrooper.idx);
+            }
+        }
+    });
+
+    return hasCollided;
+}
+
+function increaseScore(nb){
+    score += nb;
+    $('.score').html(score + ' PTS');;
+}
+
+function gameOver(){
+    // console.log("game over");
+
+    clearInterval(trooperLaunching);
+    clearInterval(scrollInterval);
+
+    $('body').off();
+
+    $('.overlay').animate({'opacity':1}, 150, function(){
+        $('.overlay h1').animate({'top':'240px'}, 150);
+    });
+}
 
 
 
--- js/space.js
+++ js/space.js
@@ -3,10 +3,10 @@
 ////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////
-// The Ship /////////////////////////////////////////////////////////////////
+// THE SHIP /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
-function Ship_obj(height, width){
+function Ship_obj(height, width, shots){
     this.ship = $('.spaceship');
     // this.height = 150;
     // this.width = 150;
@@ -16,14 +16,16 @@ function Ship_obj(height, width){
     this.shipBgLimitBottom = 1500;
     this.shipBgInit = 750;
     this.shipBgCurrent = 750;
-    this.shipTopLimit = 0;
-    this.shipBottomLimit = spaceHeight - this.height;
+    this.shipTopLimit = -25;
+    this.shipBottomLimit = spaceHeight - this.height + 25;
     this.shipLeftLimit = 0;
     this.shipRightLimit = spaceWidth - this.width;
     this.shipPosYinit = (spaceHeight - this.height)/2;
     this.shipPosXinit = 50;
-    this.shipPosYcurrent = this.shipPosYinit;
-    this.shipPosXcurrent = this.shipPosXinit;
+    this.posY = this.shipPosYinit;
+    this.posX = this.shipPosXinit;
+    this.shots = shots;
+    this.health = 4;
 }
 
 
@@ -67,37 +69,62 @@ Ship_obj.prototype.releaseBgDown = function(){
 // Animation de la position du vaisseau /////////////////////////////////////////
 
 Ship_obj.prototype.moveUp = function(){
-    if(this.shipPosYcurrent > this.shipTopLimit){
-        this.shipPosYcurrent -= 5;
-        this.ship.css({'top':this.shipPosYcurrent});
+    if(this.posY > this.shipTopLimit){
+        this.posY -= 5;
+        this.ship.css({'top':this.posY});
     }
 };
 
 Ship_obj.prototype.moveDown = function(){
-    if(this.shipPosYcurrent < this.shipBottomLimit){
-        this.shipPosYcurrent += 5;
-        this.ship.css({'top':this.shipPosYcurrent});
+    if(this.posY < this.shipBottomLimit){
+        this.posY += 5;
+        this.ship.css({'top':this.posY});
     }
 };
 
 Ship_obj.prototype.moveLeft = function(){
-    if(this.shipPosXcurrent > this.shipLeftLimit){
-        this.shipPosXcurrent -= 5;
-        this.ship.css({'left':this.shipPosXcurrent});
+    if(this.posX > this.shipLeftLimit){
+        this.posX -= 5;
+        this.ship.css({'left':this.posX});
     }
 };
 
 Ship_obj.prototype.moveRight = function(){
-    if(this.shipPosXcurrent < this.shipRightLimit){
-        this.shipPosXcurrent += 5;
-        this.ship.css({'left':this.shipPosXcurrent});
+    if(this.posX < this.shipRightLimit){
+        this.posX += 5;
+        this.ship.css({'left':this.posX});
     }
 };
 
-function Shots(shots){
-    this.shots = shots;
-    this.posX = 0;
-    this.posY = 0;
+Ship_obj.prototype.receiveDamage = function(enemy){
+    if(this.health > 0){
+        this.health -= enemy.strenght;
+        currentHealth = this.health;
+        console.log(this.health);
+        var currentLevel = 'level' + this.health;
+        var previousLevel = 'level' + (this.health+1);
+        $('.level').animate({'width':this.health*25+'%'}, 150, function(){
+            $('.level').removeClass(previousLevel).addClass(currentLevel);
+            // $('.level').removeClass('level4').addClass('level3');
+            if(currentHealth <= 0){
+                gameOver();
+            }
+        });
+    }
+};
+
+
+//////////////////////////////////////////////////////////////////////////////
+// SHOTS ////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+function Shots(strenght, posX, posY, width, height){
+    // this.shots = shots;
+    this.strenght = strenght;
+    this.posX = posX;
+    this.posY = posY;
+    this.width = width;
+    this.height = height;
 }
 
 Shots.prototype.arm = function(){
@@ -123,6 +150,7 @@ Shots.prototype.fire = function(){
     shotHeight = 4;
     // clearInterval(loadingShot);
     currentShot = shotIdx;
+    // console.log(this.shots[currentShot].strenght);
     if(shotIdx < 5){
         shotIdx += 1;
     }
@@ -131,42 +159,309 @@ Shots.prototype.fire = function(){
     }
     var thisShot = $('.armed[index='+ currentShot +']');
     thisShot.removeClass('armed');
-    var shotPosX = myShip.shipPosXcurrent+(myShip.width);
-    var shotPosY = myShip.shipPosYcurrent+(myShip.height/2);
+    var shotPosX = myShip.posX+(myShip.width);
+    var shotPosY = myShip.posY+(myShip.height/2);
     var shotWidth = 10;
     var fireShot = setInterval(function(){
         if(shotPosX <= 1000){
             shotPosX += 10;
             shotWidth += 5;
             thisShot.css({'left':shotPosX, 'width':shotWidth});
+            ammunitions[currentShot].posX = parseFloat(thisShot.css('left'));
+            ammunitions[currentShot].posY = parseFloat(thisShot.css('top'));
+            ammunitions[currentShot].width = parseFloat(thisShot.css('width'));
+            // console.log('posX : ' + this.posX);
+            // console.log('posY : ' + this.posY);
+            // console.log('Width : ' + this.width);
         }
         else{
             thisShot.css({'width':10}).addClass('armed').hide();
             clearInterval(fireShot);
         }
+
+        if(shotCollision (ammunitions[currentShot])){
+            // console.log("HIT!");
+            thisShot.css({'width':10}).addClass('armed').hide();
+            clearInterval(fireShot);
+        }
+
     }, 5);
-    
 };
 
 
+//////////////////////////////////////////////////////////////////////////////
+// ENEMIES //////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+function Enemy(enemies, strenght, health, posX, posY, width, height){
+    this.enemies = enemies;
+    this.strenght = strenght;
+    this.health = health;
+    this.posX = posX;
+    this.posY = posY;
+    this.width = width;
+    this.height = height;
+}
+
+function Trooper(strenght, health, posX, posY, idx){
+    // Enemy.call(this, strenght, health, posX, posY, width, height);
+    // Enemy.call(this, strenght, health, posX, posY);
+    this.strenght = strenght;
+    this.health = health;
+    this.posX = posX;
+    this.posY = posY;
+    this.idx = idx;
+    this.width = 70;
+    this.height = 70;
+    // this.troopers = troopers;
+}
+
+Trooper.prototype.receiveDamage = function(shot){
+    console.log("HIT!");
+    this.health -= shot.strenght;
+    // console.log(this.health);
+    return this.health;
+};
+
+// Trooper.prototype = Object.create(Enemy.prototype);
+// Trooper.prototype.constructor = Trooper;
+
+var trooper_path = [
+    { 
+    start: {
+        x: 1000,
+        y: 74,
+        angle: 33.117,
+        length: 0.780
+        },
+    end: {
+        x: -70,
+        y: 393,
+        angle: 18.449,
+        length: 0.629
+        }
+    },
+
+    {
+    start: {
+        x: 1000,
+        y: 400,
+        angle: 323.950,
+        length: 1.049
+        },
+    end: {
+        x: -70,
+        y: 41,
+        angle: 343.202,
+        length: 0.221
+        }
+    },
+
+    {
+    start: {
+        x: 1000,
+        y: 174,
+        angle: 27.846,
+        length: 0.274
+        },
+    end: {
+        x: -70,
+        y: 238,
+        angle: 310.314,
+        length: 0.269
+        }
+    },
+
+    {
+    start: {
+        x: 1000,
+        y: 389,
+        angle: 59.874,
+        length: 0.545
+        },
+    end: {
+        x: -70,
+        y: 156,
+        angle: 326.414,
+        length: 0.341
+        }
+    },
+
+    {
+    start: {
+        x: 1000,
+        y: 237,
+        angle: 1.948,
+        length: 0.356
+        },
+    end: {
+        x: -70,
+        y: 248,
+        angle: 30.882,
+        length: 0.457
+        }
+    },
+
+    {
+    start: {
+        x: 1000,
+        y: 90,
+        angle: 22.116,
+        length: 0.378
+        },
+    end: {
+        x: -70,
+        y: 394,
+        angle: 13.235,
+        length: 0.417
+        }
+    },
+
+    {
+    start: {
+        x: 1000,
+        y: 419,
+        angle: 326.185,
+        length: 0.352
+        },
+    end: {
+        x: -70,
+        y: 202,
+        angle: 39.992,
+        length: 0.516
+        }
+    },
+
+    {
+    start: {
+        x: 1000,
+        y: 271,
+        angle: 25.382,
+        length: 0.916
+        },
+    end: {
+        x: -70,
+        y: 218,
+        angle: 29.966,
+        length: 0.626
+        }
+    },
+
+    {
+    start: {
+        x: 1000,
+        y: 60,
+        angle: 14.954,
+        length: 0.933
+        },
+    end: {
+        x: -70,
+        y: 329,
+        angle: 337.873,
+        length: 0.473
+        }
+    },
+
+    {
+    start: {
+        x: 1000,
+        y: 135,
+        angle: 307.793,
+        length: 0.294
+        },
+    end: {
+        x: -70,
+        y: 228,
+        angle: 355.234,
+        length: 0.349
+        }
+    }
+];
 
 
-// function Shots(height, width){
-//     this.height = height;
-//     this.width = width;
-//     this.shot = $('.shot');
-//     this.html = $('<div class="shot"></div>');
-//     this.shotX = myShip.shipPosXcurrent;
-//     this.shotY = myShip.shipPosYcurrent;
-// }
+Trooper.prototype.launch = function(x, y){
+    var currentTrooper = trooperIdx;
+    // spaceBg.append('<div class="enemy trooper"></div>');
+    var thisTrooper = $('.trooper[index='+ trooperIdx +']');
+    thisTrooper.css({'left': x, 'top': y}).show();
+    // console.log(thisTrooper.css('left'));
+    // console.log(thisTrooper.css('width'));
+    // console.log(trooperSection[currentTrooper]);
+    // var trooperExplosion;
+    var thisTrooperLaunch = setInterval(function(){
+        trooperSection[currentTrooper].posX = parseFloat(thisTrooper.css('left'));
+        trooperSection[currentTrooper].posY = parseFloat(thisTrooper.css('top'));
+        // console.log('posX : ' + trooperSection[currentTrooper].posX);
+        // console.log('posY : ' + trooperSection[currentTrooper].posY);
+        // console.log(trooperSection[currentTrooper]);
+        if(trooperCollision (trooperSection[currentTrooper])){
+            console.log("CRASH");
+            clearInterval(thisTrooperLaunch);
+            clearInterval(thisTrooperBgAnim);
+            // thisTrooper.hide();
+            // myShip.health -= 1;
+            // console.log(myShip.health);
+            myShip.receiveDamage(trooperSection[currentTrooper]);
 
-// Shots.prototype.move = function(){
-//     var shotPosition = setInterval(function(){
-//         shot.shot.css({'top':myShip.shipPosYcurrent+(myShip.height/2), 'left':myShip.shipPosXcurrent+(myShip.width)});
-//     }, 8);
-// };
+            // trooperBgY = 0;
+            // thisTrooper.css({'background-position-x':-70, 'background-position-y':0});
+            // trooperExplosion = setInterval(function(){
+            //     thisTrooper.css({'background-position-y':trooperBgY});
+            //     trooperBgY -= 70;
+            //     if(trooperBgY <= -1190){
+            //         clearInterval(trooperExplosion);
+            //         thisTrooper.hide();
+            //     }
+            // },60);
+
+            thisTrooper.hide();
+            
+            trooperExplosion(currentTrooper);
+            
+            // var explosion = '<div index="' + currentTrooper + '" class="explosion"></div>';
+            // $('.explosionsLayer').append(explosion);
+            // explosion = $('.explosion[index='+ currentTrooper +']');
+            // explosion.css({'left':trooperSection[currentTrooper].posX-35, 'top':trooperSection[currentTrooper].posY-35});
+            // var explosionBg = 0;
+            // var trooperExplosion = setInterval(function(){
+            //     explosion.css({'background-position-y':explosionBg});
+            //     explosionBg -= 140;
+            //     if(explosionBg <= -2380){
+            //         clearInterval(trooperExplosion);
+            //         explosion.remove();
+            //     }
+            // },60);
+
+        }   
 
 
+    }, 10);
+
+    var trooperBgY = 0;
+    var thisTrooperBgAnim = setInterval(function(){
+        thisTrooper.css({'background-position-y':trooperBgY});
+        trooperBgY -= 70;
+        if(trooperBgY <= -1190){
+            trooperBgY = 0;
+        }
+    }, 60);
+
+    var randPath = Math.floor(Math.random()*10);
+
+    setTimeout(function(){
+        clearInterval(thisTrooperLaunch);
+        clearInterval(thisTrooperBgAnim);
+    }, 4000);
+
+    thisTrooper.animate({path : new $.path.bezier(trooper_path[randPath])}, 4000, function(){
+        // clearInterval(thisTrooperLaunch);
+        // clearInterval(thisTrooperBgAnim);
+        thisTrooper.css({'left': 1000, 'top': -70, 'background-position-y':0, 'background-position-x':0}).hide();
+        trooperSection[currentTrooper].posX = 1000;
+        trooperSection[currentTrooper].posY = -70;
+        trooperSection[currentTrooper].health = 20;
+    });
+};
 
 
 
